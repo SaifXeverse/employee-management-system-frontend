94d8f9d5e5dc300c6aa2c12a8a93492efcaa08c3
SaifXeverse <saifullah.k@xeverse.io>
2026-07-20 19:40:49 +0500

    adjust resume button

--- components/admin/employee/EmployeeDetails.tsx
+++ components/admin/employee/EmployeeDetails.tsx
@@ -197,26 +197,26 @@ const EmployeeDetails = () => {
                     href={employee.resume}
                     target="_blank"
                     prefetch={false}
-                    className="inline-flex items-center gap-2 rounded-xl bg-blue-600 px-2 sm:px-5 py-2 sm:py-3 font-medium text-white transition-all duration-300 hover:bg-blue-700 hover:-translate-y-0.5 hover:shadow-lg"
+                    className="inline-flex items-center gap-1 sm:gap-2 rounded-xl bg-blue-600 px-2 sm:px-5 py-2 sm:py-3 font-medium text-white transition-all duration-300 hover:bg-blue-700 hover:-translate-y-0.5 hover:shadow-lg"
                   >
-                    <Eye size={18} />
+                    <Eye className="w-3.5 sm:w-4.5" />
                     View
                   </Link>
                   <Link
                     href={getAttachmentUrl(employee?.resume || "")}
                     prefetch={false}
-                    className="inline-flex items-center gap-2 rounded-xl border border-blue-600 bg-white px-2 sm:px-5 py-2 sm:py-3 font-medium text-blue-600 transition-all duration-300 hover:bg-blue-50 hover:-translate-y-0.5 hover:shadow-lg"
+                    className="inline-flex items-center gap-1 sm:gap-2 rounded-xl border border-blue-600 bg-white px-2 sm:px-5 py-2 sm:py-3 font-medium text-blue-600 transition-all duration-300 hover:bg-blue-50 hover:-translate-y-0.5 hover:shadow-lg"
                   >
-                    <Download size={18} />
+                    <Download className="w-3.5 sm:w-4.5" />
                     Download
                   </Link>
                   <button
                     onClick={() =>
                       dispatch(deleteEmployeeResumeByAdmin(employee.id!))
                     }
-                    className="inline-flex items-center gap-2 rounded-xl bg-red-500 px-2 sm:px-5 py-2 sm:py-3 font-medium text-white transition-all duration-300 hover:bg-red-600 hover:-translate-y-0.5 hover:shadow-lg"
+                    className="inline-flex items-center gap-1 sm:gap-2 rounded-xl bg-red-500 px-2 sm:px-5 py-2 sm:py-3 font-medium text-white transition-all duration-300 hover:bg-red-600 hover:-translate-y-0.5 hover:shadow-lg"
                   >
-                    <Trash2 size={18} />
+                    <Trash2 className="w-3.5 sm:w-4.5" />
                     Delete
                   </button>
                 </div>
